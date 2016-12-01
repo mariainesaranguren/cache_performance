0ab001b08185bdb7425ef2030d6d86185f09561e
mariainesaranguren <marangu1@nd.edu>
2016-12-01 18:12:06 -0500

    Initial commit

--- block_size_isimp_runs.py
+++ block_size_isimp_runs.py
@@ -6,54 +6,63 @@ import math
 import os.path
 import time
 
+# List of lists (will have x and y for each capacity)
+# block_size_all = []
+miss_rate_all = []
 
-block_size_vals = []
-miss_rate_vals = []
-
-block_size_all = [] # list of block_size_vals of each capacity
-miss_rate_all = [] # list of miss_rate_vals of each capacity
-all_capacities = [32, 64]  # , 128, 256, 512, 1024, 2048, 4096
-
-# for capacity in all_capacities:
-capacity = 32
-# for associativity in 1:
+capacities_all = [32, 64, 128, 256, 512, 1024, 2048, 4096]
 associativity = 1
-for block_size in [2, 4, 8, 16, 32]:
-    # capacity = associativity * sets * block_size
-    sets = capacity/(associativity * block_size)
-    cmd = ('isimp -dcache ' +
-            str(associativity) + ' ' +
-            str(int(math.log(sets, 2))) + ' ' +
-            str(int(math.log((block_size * 4),2))) +
-            ' a.out')
+block_size_vals = [1, 2, 4, 8, 16, 32] # list of block_size_vals of each capacity
+miss_rate_vals = [] # list of miss_rate_vals of each capacity
 
-    print cmd
-    os.system(cmd)
-
-    f = open('stats.csv')
-    reader = csv.reader(f)
-    my_dict = dict(reader)
-    miss_rate = float(my_dict['dCacheMissRate'])
-    block_size_vals.append(block_size)
-    miss_rate_vals.append(miss_rate)
-    print "capacity: ", capacity, "miss_rate: ", miss_rate, "block_size: ", block_size
-
-block_size_all.append(block_size_vals)
-miss_rate_all.append(miss_rate_vals)
-
+for capacity in capacities_all:
+    for block_size in block_size_vals:
+        sets = capacity/(associativity * block_size)
+        print "capacity: ", capacity
+        cmd = ('isimp -dcache ' + str(associativity) + ' ' + str(int(math.log(sets, 2))) + ' ' + str(int(math.log((block_size * 4),2))) + ' a.out')
+        print cmd
+        os.system(cmd)
+        f = open('stats.csv')
+        reader = csv.reader(f)
+        my_dict = dict(reader)
+        miss_rate = float(my_dict['dCacheMissRate'])
+        block_size_vals.append(block_size)
+        miss_rate_vals.append(miss_rate)
+        print "capacity: ", capacity, "miss_rate: ", miss_rate, "block_size: ", block_size
+    miss_rate_all.append(miss_rate_vals)
+    # Clear lists
+    block_size_vals = []
+    miss_rate_vals = []
+    print "DASDASDASDAS"
+    # Put results into list of list
+    # block_size_all.append(block_size_vals)
 
 # Plotting
-x = np.arange(0, 3 * np.pi, 0.1)
+x = np.array(block_size_vals)
 print x
-x = block_size_all
-print x
-y = miss_rate_all
+y = np.array(miss_rate_vals)
 print y
-plt.plot(x, y)
+plt.plot(x, y, '-o')
+plt.xscale('log', basex=2)
 plt.xlabel('Block Size (words)')
 plt.ylabel('Miss Rate')
 plt.title('Miss Rate vs. Block Size and Capacity (in words)')
-plt.legend(all_capacities)
 plt.show()
 
-# print x
+for capacity in capacities_all:
+    x = np.array(block_size_vals)
+    y = np.array(miss_rate_all[capacity])
+    # print y
+    plt.plot(x, y, '-o')
+plt.xscale('log', basex=2)
+plt.xlabel('Block Size (words)')
+plt.ylabel('Miss Rate')
+plt.title('Miss Rate vs. Block Size and Capacity (in words)')
+plt.legend(capacities_all)
+plt.show()
+
+
+
+
+
+#
